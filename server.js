f9ee6d638d52336f5efc603c7fc224f317f1e972
Elias Mason <eliasjmason@gmail.com>
2017-03-10 08:55:45 -0800

    Cleans up comments

--- server.js
+++ server.js
@@ -18,53 +18,62 @@ const router = express.Router();
 
 
 
-// serve html
+/* Serve html */
 
 router.get('/', (req, res) => {
   
-   /**
-     set options on Express's res.locals object:
+   /** Set options on Express's res.locals object, which lets us add data
+    * to a response:
+    *
+    * If the name form was sent, the name prop will be set to that name
+    * and displayed in the view; if not, it will be undefined,
+    * and our template will ignore it.
 
-     If the name form was sent, the name prop will be set to that name
-     and displayed in the view; if not, it will be undefined,
-     and our template will ignore it.
-
-     If the form for enabling JS was sent, 
-     app.locals.jsEnabled will be true
-     and script tags will be added to the DOM.
+    * If the form for enabling JS was sent, app.locals.jsEnabled 
+    * will be true and script tags will be added to the DOM.
+    * @example
+    * // renders 'You submitted "Wizard Ben"` and enables JavaScript
+    * res.locals = {
+    *   name: 'Wizard Ben',
+    *   jsEnabled: true
+    * }
     */
-  
   res.locals = {
     name: req.query.name ? req.query.name : undefined,
     jsEnabled: app.locals.jsEnabled
   }
 
+  /** Pass the locals object into the render function
+   * to expose variables to our template.
+   */
 
-  // Passing the locals object into the render function
-  // is what allows us to expose variables to our template.
-  res.render('home', res.locals);
+    res.render('home', res.locals); 
 });
 
 router.post('/print-name', (req, res) => {
 
-  // send user's name as query string so it can be printed on redirect
+  /* Send user's name as query string so it can be printed on redirect */
   res.redirect(302, '/?name=' + req.body.name);
 });
 
 router.post('/toggle-js', (req, res) => {
 
-  /** 
-  The toggle-js radios have values of 1 and 0,
-  which can also be evaluated to true or false, respectively.
-  Here, we convert those numbers to their corresponding booleans
-  and assign that boolean to the jsEnabled prop of the app.locals object
-  that is built into Express.
-  */
+  /** Set jsEnabled on the app.locals object and redirect to home.
+   * The app.locals object is like res.locals, except its data is "globa"
+   * to the whole app, and not just one req-res cycle.
+   * The toggle-js radios have values of 1 and 0,
+   * which can also be evaluated to true or false, respectively.
+   * 
+   * Here, we convert 1 or 0 to its corresponding boolean and assign 
+   * that boolean to the jsEnabled prop of the app.locals object.
+   * That boolean is then sent as a query string so that handlebars
+   * can do something with it.
+   */
   app.locals.jsEnabled = (req.body['js-enabled'] == true);
   res.redirect(302, '/?js-enabled=' + app.locals.jsEnabled)
 });
 
-// handle navigation to nonexistant routes
+/* handle navigation to nonexistant routes */
 
 router.use('*', function(req, res) {
   return res.status(404).json({message: 'Not Found'});
