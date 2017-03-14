e6c3f5bb689d06f069ae54a278f9f38ca87bffbf
Elias Mason <eliasjmason@gmail.com>
2017-03-14 13:39:05 -0700

    Cleans up comments

--- server.js
+++ server.js
@@ -27,9 +27,10 @@ router.get('/', (req, res) => {
    /** Set options on Express's app.locals object, which lets us carry data
     * in the server app. This will be our `database`, for lack of a real one.
     *
-    * If the name form was sent, the app.locals.name prop will be set to that name
+    * If the name form was sent, the app.locals.name prop will have that name
     * and displayed in the view; if not, it will be undefined,
     * and our template will ignore it.
+
     * If the form for enabling JS was sent, app.locals.jsEnabled 
     * will be true and script tags will be added to the DOM.
     */    
@@ -44,8 +45,7 @@ router.get('/', (req, res) => {
 router.post('/print-name', (req, res) => {
 
   /** Send user's name as query string just to illustrate 
-   * *that stuff has happened on the server 
-   * TODO: use req.xhr to test if ajax request and handle sending actual data
+   * that routing has happened on the server.
    */
   app.locals.name = req.body.name;
 
@@ -59,21 +59,21 @@ router.post('/print-name', (req, res) => {
 router.post('/toggle-js', (req, res) => {
 
   /** Set jsEnabled on the app.locals object and redirect to home.
-   * The app.locals object is like res.locals, except its data is "globa"
-   * to the whole app, and not just one req-res cycle.
+
    * The toggle-js radios have values of 1 and 0,
    * which can also be evaluated to true or false, respectively.
    * 
-   * Here, we convert 1 or 0 to its corresponding boolean and assign 
-   * that boolean to the jsEnabled prop of the app.locals object.
-   * That boolean is then sent as a query string so that handlebars
-   * can do something with it.
+   * Here, we get the 1 or 0 from the req.body and convert it to 
+   * its corresponding boolean. That boolean is then set to app.locals.jsEnabled.
+   * 
+   * Then we send the 1 or 0 in the query string as a visual indicator that we have
+   * done some navigating server-side.
    */
   app.locals.jsEnabled = (req.body['js-enabled']==true);  
   res.redirect(301, '/?js-enabled=' + req.body['js-enabled']);
 });
 
-/* handle navigation to nonexistant routes */
+/* Handle navigation to nonexistant routes */
 
 router.use('*', function(req, res) {
   return res.status(404).json({message: 'Not Found'});
