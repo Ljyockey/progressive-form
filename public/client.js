8d3c538af19b00be7e202379bc3d946e468d14e7
Elias Mason <eliasjmason@gmail.com>
2017-03-14 15:29:18 -0700

    Adds comemnts to walk through client

--- public/client.js
+++ public/client.js
@@ -23,17 +23,31 @@ $(function() {
 
   checkStorage();
 
+    /** Store whether or not JS is active when the selected input
+   * within the form is changed. The change will be finalized when
+   * the form is submitted to the server.
+   */
   $toggleForm.change(function(e){
     populateStorage($('input:checked'));
   });
 
+
   $nameForm.submit(e => {
     e.preventDefault();
+    /** e.target is the element that triggered the event we're handling.
+     * It's a nice way to refer back to the appropriate element.
+     * We wrap it in a jQuery object so we can do jQuery things to it.
+     * 
+     * ...like capture its 'action' prop, then serialize it.
+     */
 
     var url = $(e.target).prop('action');
     var formData = $(e.target).serializeArray();
 
     $.post(url, formData).done(function(data){
+      /** At this point, the data is simply the name we submitted, 
+       * so we can make a DOM node and put the text in.
+       */
       var $nameNode = $('<p>', {text: data});
       $outputSection.append($nameNode);
       $nameField.val('');
